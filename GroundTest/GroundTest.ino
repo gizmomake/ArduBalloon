9135da88ef4ce44beb95dfcd354d8cce6fe209f1
Ben Thomson <ben.m.thomson@gmail.com>
2013-11-11 18:02:01 -0500

    added groundtest

---
+++ GroundTest/GroundTest.ino
@@ -0,0 +1,312 @@
+
+/*
+   _____ _____   ____  _    _ _   _ _____    _______ ______  _____ _______ 
+  / ____|  __ \ / __ \| |  | | \ | |  __ \  |__   __|  ____|/ ____|__   __|
+ | |  __| |__) | |  | | |  | |  \| | |  | |    | |  | |__  | (___    | |   
+ | | |_ |  _  /| |  | | |  | | . ` | |  | |    | |  |  __|  \___ \   | |   
+ | |__| | | \ \| |__| | |__| | |\  | |__| |    | |  | |____ ____) |  | |   
+  \_____|_|  \_\\____/ \____/|_| \_|_____/     |_|  |______|_____/   |_|   
+                                                                         
+                           DO NOT FLY  
+                                                                           
+                                                                           
+                                                                           */
+#include <SoftwareSerial.h>
+#include <String.h>
+#include <TinyGPS.h>
+#include <Wire.h>
+#include "DHT.h"
+
+
+// For Texting
+SoftwareSerial mySerial(7, 8);
+//For GPS
+TinyGPS gps;
+SoftwareSerial nss(2, 3);
+static void gpsdump(TinyGPS &gps);
+static bool feedgps();
+static void print_float(float val, float invalid, int len, int prec);
+static void print_int(unsigned long val, unsigned long invalid, int len);
+static void print_date(TinyGPS &gps);
+static void print_str(const char *str, int len);
+int i = true;
+
+//For temp and humi sensor
+#define DHTPIN A0
+#define DHTTYPE DHT11   // DHT 11 
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+    Serial.begin(19200);
+    dht.begin();
+    
+
+
+}
+void loop()
+{
+  
+    // Reading temperature or humidity takes about 250 milliseconds!
+  float h = dht.readHumidity();
+  float nt = dht.readTemperature();
+  float t = nt * 9/5 + 32;
+  
+  if (i) {
+    Serial.begin(19200);
+    nss.begin(9600);
+      
+  bool newdata = false;
+  unsigned long start = millis();
+  
+  // Every second we print an update
+  while (millis() - start < 1000)
+  {
+    if (feedgps())
+      newdata = true;
+  }   
+    
+   nss.end();
+   Serial.end();
+  }
+  
+  if (!i) {
+    
+   Serial.begin(19200);
+   mySerial.begin(19200);
+
+
+
+  float flat, flon;
+  unsigned long age, date, time, chars = 0;
+  unsigned short sentences = 0, failed = 0;
+  
+  //////////////////////////////////////////
+  //     Print GPS data to serial        //
+  //    MUST BE REMOVED BEFORE FLIGHT   //
+  ///////////////////////////////////////  
+  
+  print_int(gps.satellites(), TinyGPS::GPS_INVALID_SATELLITES, 5);
+  print_int(gps.hdop(), TinyGPS::GPS_INVALID_HDOP, 5);
+  gps.f_get_position(&flat, &flon, &age);
+  print_float(flat, TinyGPS::GPS_INVALID_F_ANGLE, 9, 5);
+  print_float(flon, TinyGPS::GPS_INVALID_F_ANGLE, 10, 5);
+  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
+
+  print_date(gps);
+
+  print_float(gps.f_altitude(), TinyGPS::GPS_INVALID_F_ALTITUDE, 8, 2);
+  print_float(gps.f_course(), TinyGPS::GPS_INVALID_F_ANGLE, 7, 2);
+  print_float(gps.f_speed_kmph(), TinyGPS::GPS_INVALID_F_SPEED, 6, 2);
+  print_str(gps.f_course() == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(gps.f_course()), 6);
+ // print_int(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0UL : (unsigned long)TinyGPS::distance_between(flat, flon, LONDON_LAT, LONDON_LON) / 1000, 0xFFFFFFFF, 9);
+  print_float(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : TinyGPS::course_to(flat, flon, 51.508131, -0.128002), TinyGPS::GPS_INVALID_F_ANGLE, 7, 2);
+ // print_str(flat == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(TinyGPS::course_to(flat, flon, LONDON_LAT, LONDON_LON)), 6);
+
+  gps.stats(&chars, &sentences, &failed);
+  print_int(chars, 0xFFFFFFFF, 6);
+  print_int(sentences, 0xFFFFFFFF, 10);
+  print_int(failed, 0xFFFFFFFF, 9);
+  Serial.println();
+  
+  
+
+  
+  ///////////////
+  // Send text //
+  ///////////////
+ 
+  
+  mySerial.print("AT+CMGF=1\r");
+  delay(100);
+  mySerial.println("AT + CMGS = \"+15163984586\"");//send sms message, be careful need to add a country code before the cellphone number
+  delay(100);
+  
+
+  /////////
+  // LAT //
+  /////////
+  
+  float val = flat;
+  float invalid = TinyGPS::GPS_INVALID_F_ANGLE;
+  int len = 9;
+  int prec = 5;
+  char sz[32];
+  if (val == invalid)
+  {
+    strcpy(sz, "*******");
+    sz[len] = 0;
+        if (len > 0) 
+          sz[len-1] = ' ';
+    for (int i=7; i<len; ++i)
+        sz[i] = ' ';
+    Serial.print(sz);
+  }
+  else
+  {
+    mySerial.print(val, prec);
+    int vi = abs((int)val);
+    int flen = prec + (val < 0.0 ? 2 : 1);
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+    for (int i=flen; i<len; ++i)
+      mySerial.print(" , ");
+  }
+  
+  delay(100);
+  
+  /////////
+  // LON //
+  /////////
+  
+  float val1 = flon;
+  float invalid1 = TinyGPS::GPS_INVALID_F_ANGLE;
+  int len1 = 9;
+  int prec1 = 5;
+  char sz1[32];
+  if (val1 == invalid1)
+  {
+    strcpy(sz1, "*******");
+    sz1[len1] = 0;
+        if (len1 > 0) 
+          sz1[len1-1] = ' ';
+    for (int i=7; i<len1; ++i)
+        sz1[i] = ' ';
+    Serial.print(sz1);
+  }
+  else
+  {
+    mySerial.print(val1, prec1);
+    int vi = abs((int)val1);
+    int flen = prec1 + (val1 < 0.0 ? 2 : 1);
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+    for (int i=flen; i<len; ++i)
+      mySerial.print(" ALT: ");
+  }
+  delay(100);
+  mySerial.println(" ALT: ");
+  /////////
+  // ALT //
+  /////////
+  
+  float val2 = gps.f_altitude();
+  float invalid2 = TinyGPS::GPS_INVALID_F_ALTITUDE;
+  int len2 = 8;
+  int prec2 = 2;
+  char sz2[32];
+  if (val2 == invalid2)
+  {
+    strcpy(sz2, "*******");
+    sz2[len2] = 0;
+        if (len2 > 0) 
+          sz2[len2-1] = ' ';
+    for (int i=7; i<len2; ++i)
+        sz2[i] = ' ';
+    Serial.print(sz2);
+  }
+  else
+  {
+    mySerial.print(val2, prec2);
+    int vi = abs((int)val2);
+    int flen = prec2 + (val2 < 0.0 ? 2 : 1);
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+    for (int i=flen; i<len2; ++i)
+      mySerial.print(" ");
+  }
+  delay(100);
+  
+  mySerial.println((char)26);//the ASCII code of the ctrl+z is 26
+  delay(100);
+  
+  mySerial.println();
+  
+  mySerial.end();
+  
+  Serial.end();
+  
+  }
+ 
+    i = !i;
+    //wait 5 seconds
+   delay(5000);
+
+}
+
+
+static void print_int(unsigned long val, unsigned long invalid, int len)
+{
+  char sz[32];
+  if (val == invalid)
+    strcpy(sz, "*******");
+  else
+    sprintf(sz, "%ld", val);
+  sz[len] = 0;
+  for (int i=strlen(sz); i<len; ++i)
+    sz[i] = ' ';
+  if (len > 0) 
+    sz[len-1] = ' ';
+  Serial.print(sz);
+  feedgps();
+}
+
+static void print_float(float val, float invalid, int len, int prec)
+{
+  char sz[32];
+  if (val == invalid)
+  {
+    strcpy(sz, "*******");
+    sz[len] = 0;
+        if (len > 0) 
+          sz[len-1] = ' ';
+    for (int i=7; i<len; ++i)
+        sz[i] = ' ';
+    Serial.print(sz);
+  }
+  else
+  {
+    Serial.print(val, prec);
+    int vi = abs((int)val);
+    int flen = prec + (val < 0.0 ? 2 : 1);
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+    for (int i=flen; i<len; ++i)
+      Serial.print(" ");
+  }
+  feedgps();
+  
+}
+
+static void print_date(TinyGPS &gps)
+{
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+  unsigned long age;
+  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
+  if (age == TinyGPS::GPS_INVALID_AGE)
+    Serial.print("*******    *******    ");
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d   ",
+        month, day, year, hour, minute, second);
+    Serial.print(sz);
+  }
+  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
+  feedgps();
+}
+
+static void print_str(const char *str, int len)
+{
+  int slen = strlen(str);
+  for (int i=0; i<len; ++i)
+    Serial.print(i<slen ? str[i] : ' ');
+  feedgps();
+}
+
+static bool feedgps()
+{
+  while (nss.available())
+  {
+    if (gps.encode(nss.read()))
+      return true;
+  }
+  return false;
+}
